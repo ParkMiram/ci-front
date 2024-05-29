a1f095b0093bd36354ee670db4bae0848c7de7b5
ParkMiram <myurm@naver.com>
2024-05-29 10:54:20 +0900

    host 수정

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: `http://104.154.175.41:8080/api`
+    baseURL: `http://${window.location.host}:8080/api`
 });
 
 export default axiosInstance;
