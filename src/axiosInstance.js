c797d2da605c13268341a26848c643a5ffe789b1
ParkMiram <myurm@naver.com>
2024-05-30 12:06:55 +0900

    asd

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL: `http://${window.location.host}:8080/api`
+    // baseURL: `http://${window.location.host}:8080/api`
+    // baseURL: `http://104.154.175.41:8080/api`
+    baseURL: `/api`
 });
 
 export default axiosInstance;
